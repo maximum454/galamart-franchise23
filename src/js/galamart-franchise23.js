b6ecd953550929c97d958aabf24304ef21800c35
Maxim Karachyov <karachyov.m@gmail.com>
2023-02-15 13:00:10 +0300

    franchise23-reviews

--- src/js/galamart-franchise23.js
+++ src/js/galamart-franchise23.js
@@ -1,2 +1,14 @@
 @@include('./partials/swiper-bundle.min.js')
 
+const swiperReviews = new Swiper('.swiper-reviews', {
+    slidesPerView: 1,
+    spaceBetween: 85,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
